057563e82fe8ff4a0ca9294b93fd40ef61093ee6
Paul Quiroz <pquiroz1977@gmail.com>
2018-05-07 15:31:40 -0700

    complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -24,11 +24,18 @@ handleChange(filter){
 
 
   render() {
+
     let foodsToDisplay = this.state.foods.filter((element,index) => {
+
       return element.includes( this.state.filterString);
+   
     }).map((element, index) => {
+
       return <h2 key= { index }>{ element }</h2>
+
     })
+
+console.log(foodsToDisplay)
     return (
       <div className='App'>
         <input onChange={ e => this.handleChange(e.target.value)} type='text'/>
